70c1968b2fbbcbcaf044a813c7ab0531ec8b35bd
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-16 14:25:44 +0530

    tested max value for float

--- src/com/generics/max/MaxTest.java
+++ src/com/generics/max/MaxTest.java
@@ -6,21 +6,21 @@ import org.junit.Test;
 public class MaxTest {
 
 	@Test
-	public void integerMax_AtFirstPosition() {
-        int actualResult = Maximum.findMaximum(10,8,6);
-        Assert.assertEquals(10,actualResult);
+	public void floatMax_AtFirstPosition() {
+        float actualResult = Maximum.findMaximum(8.6f,7.42f,3.4f);
+        Assert.assertEquals(8.6f,actualResult,0.0);
     }
 	
 	@Test
-	public void integerMax_AtSecondPosition() {
-        int actualResult = Maximum.findMaximum(8,10,6);
-        Assert.assertEquals(10,actualResult);
+	public void floatMax_AtSecondPosition() {
+        float actualResult = Maximum.findMaximum(7.42f,8.6f,3.4f);
+        Assert.assertEquals(8.6f,actualResult,0.0);
     }
 	
 	@Test
-	public void integerMax_AtThirdPosition() {
-        int actualResult = Maximum.findMaximum(8,6,10);
-        Assert.assertEquals(10,actualResult);
+	public void floatMax_AtThirdPosition() {
+        float actualResult = Maximum.findMaximum(3.4f,7.42f,8.6f);
+        Assert.assertEquals(8.6f,actualResult,0.0);
     }
 
 }
--- src/com/generics/max/Maximum.java
+++ src/com/generics/max/Maximum.java
@@ -2,15 +2,15 @@ package com.generics.max;
 
 public class Maximum {
 	
-	public static Integer findMaximum(Integer a, Integer b, Integer c) {
-		Integer max = a;
+	public static Float findMaximum(Float a, Float b, Float c) {
+		Float max = a;
 		if(b.compareTo(max) > 0 && b.compareTo(c) > 0) {
 			max = b;
 		}
 		else if(c.compareTo(max) > 0) {
 			max = c;
 		}
-		System.out.println("Maximum Integer value : " +max);
+		System.out.println("Maximum Float value : " +max);
 		return max;
 	}
 	
