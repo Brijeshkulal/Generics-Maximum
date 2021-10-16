ed297923048c0329ea2e0002ec07dc38d1170d71
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-16 13:05:46 +0530

    Added max value of integer

--- src/com/generics/max/Maximum.java
+++ src/com/generics/max/Maximum.java
@@ -1,8 +1,22 @@
 package com.generics.max;
 
 public class Maximum {
+	
+	public static Integer findMaximum(Integer a, Integer b, Integer c) {
+		Integer max = a;
+		if(b.compareTo(max) > 0 && b.compareTo(c) > 0) {
+			max = b;
+		}
+		else if(c.compareTo(max) > 0) {
+			max = c;
+		}
+		System.out.println(max);
+		return max;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Generics to calculate Maximum Program");
+		System.out.println("Welcome to Generics to calculate Maximum Value");
+		Maximum.findMaximum(10,4,12);
 	}
 
 }
