275f54dc8120be93cdbd9375ee3179856d1b6e40
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-16 16:28:41 +0530

    Refactored into generic method

--- src/com/generics/max/MaxTest.java
+++ src/com/generics/max/MaxTest.java
@@ -6,19 +6,19 @@ import org.junit.Test;
 public class MaxTest {
 
 	@Test
-	public void integerMax_AtFirstPosition() {
+	public void integerMax() {
         int actualResult = Maximum.findMaximum(8,10,6);
         Assert.assertEquals(10,actualResult);
     }
 
 	@Test
-	public void floatMax_AtSecondPosition() {
+	public void floatMax() {
         float actualResult = Maximum.findMaximum(7.42f,8.6f,3.4f);
         Assert.assertEquals(8.6f,actualResult,0.0);
     }
 
     @Test
-    public void floatMax_AtThirdPosition() {
+    public void StringMax() {
         String actualResult = Maximum.findMaximum("Apple","Banana","Peach");
         Assert.assertEquals("Peach",actualResult);
     }
